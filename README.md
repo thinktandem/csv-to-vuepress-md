0db4ded2482771733e35fb5714b0b19e38764e7b
John Ouellet <john@thinktandem.io>
2019-09-20 15:01:13 -0400

    Add file to repo

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# csv-to-vuepress-md
-Converts a CSV to VuePress Markdown via Symfony Console CLI
+# CSV to VuePress Markdown
+
+A Symfony Console Command that create a command to generate VuePress Markdown files from CSVs.
+
+## To Use
+
+```markdown
+git clone git@github.com:thinktandem/csv-to-vuepress-md.git
+cd csv-to-vuepress-md
+composer install
+bin/console generate:md FILE_PATH
+```
+
+### Requirements
+
+The file path is required. It will error out without it.
+
+Your CSV **MUST** have a title, url, & body columns and header in it for this to work,
+
+### Questions
+
+Currently, we ask 2 questions
+
+1. What delimiter do you want to use (defaults to ,)
+2. Do you want to process textile formatted body field text (defaults to FALSE)
+
+If you do process textile text, it will convert it to HTML first then to markdown.
+
+### YAML Frontmatter
+
+All columns that are not title, url, & body will be generated as [YAML Frontmatter](https://vuepress.vuejs.org/guide/frontmatter.html#predefined-variables).
+
+### Content
+
+Textile text (if chosen) is converted to HTML via [PHP-Textile](https://github.com/textile/php-textile).
+
+Most HTML is converted to Markdown & Markdown Extra via [Markdownify](https://github.com/Elephant418/Markdownify).
+
+### Output
+
+Currently all files are generated to the output folder.  You can just copy the contents of that into your VuePress setup.
---
+++ src/Command/GenerateMdCommand.php
@@ -0,0 +1,224 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Markdownify\ConverterExtra;
+use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+use Netcarver\Textile\Parser;
+
+class GenerateMdCommand extends Command {
+
+  /**
+   * @var string
+   */
+  protected static $defaultName = 'generate:md';
+
+  /**
+   * @var array
+   */
+  protected $header = [];
+
+  /**
+   * @var array
+   */
+  protected $rows = [];
+
+  /**
+   * @var array
+   */
+  protected $currentRow;
+
+  /**
+   * @var string
+   */
+  protected $urlKey;
+
+  /**
+   * @var string
+   */
+  protected $bodyKey;
+
+  /**
+   * @var string
+   */
+  protected $titleKey;
+
+  /**
+   * @var string
+   */
+  protected $filePath;
+
+  /**
+   * @var bool
+   */
+  protected $textileFormatted = FALSE;
+
+  /**
+   * @inheritdoc
+   */
+  protected function configure() {
+      $this
+        ->setDescription('Generates a VuePress MD file from a CSV')
+        ->addArgument('filename', InputArgument::REQUIRED, 'Path to CSV File (ex: ./fancy.csv)');
+  }
+
+  /**
+   * @inheritdoc
+   */
+  protected function execute(InputInterface $input, OutputInterface $output) {
+    // Start the helped
+    $helper = $this->getHelper('question');
+
+    // Ask deliminator question
+    $dquestion = new Question('CSV delimiter (defaults to ,): ', ',');
+    $delim = $helper->ask($input, $output, $dquestion);
+
+    // Ask the textile question.
+    $tquestion = new ConfirmationQuestion('Process body field from textile format (y/n): ', FALSE);
+    $this->textileFormatted = $helper->ask($input, $output, $tquestion);
+
+    $io = new SymfonyStyle($input, $output);
+    if ($filename = $input->getArgument('filename')) {
+      if (($handle = fopen($filename, "rb")) !== FALSE) {
+        $i = 0;
+        while (($data = fgetcsv($handle, null, $delim)) !== FALSE) {
+          $i++;
+          if ($i === 1) {
+            $this->header = $data;
+            continue;
+          }
+          $this->rows[] = $data;
+        }
+        fclose($handle);
+      }
+
+      // Make sure we have body and url fields.
+      if (!$this->validateHeader()) {
+        $io->error("Your CSV file does not contain the required header keys of body, title, and/or url");
+        return;
+      }
+
+      // Make sure we got stuffs to process.
+      if (empty($this->rows)) {
+        $io->error("Your CSV contains no data");
+        return;
+      }
+
+      // Engage.
+      $this->processRows();
+      $io->success("All files have been generated to the ./output directory");
+    }
+    else {
+      $io->error("Please provide a filename or pipe template content to STDIN.");
+      return;
+    }
+  }
+
+  /**
+   * Check is the required header keys are there.
+   *
+   * @return bool
+   */
+  protected function validateHeader() {
+    return count(array_intersect($this->header, ['body', 'url', 'title'])) == count(['body', 'url', 'title']);
+  }
+
+  /**
+   * Processes the Rows of stuffs.
+   */
+  protected function processRows() {
+    $this->bodyKey = array_search('body', $this->header);
+    $this->urlKey = array_search('url', $this->header);
+    $this->titleKey = array_search('title', $this->header);
+
+    // Engage.
+    foreach ($this->rows as $row) {
+      $this->currentRow = $row;
+      $this->createDirectoryStructureAndFile();
+      $this->generateFrontMatter();
+      $this->generateContent();
+    }
+  }
+
+  /**
+   * Creates the directories and files.
+   */
+  protected function createDirectoryStructureAndFile() {
+    // Munge on our path.
+    $path = explode('/', $this->currentRow[$this->urlKey]);
+    $output = './output/';
+
+    // Generate the file name.
+    $file = (count($path) > 1) ? array_pop($path) : 'README';
+
+    // Set the dir path.
+    $output .= implode('/', $path);
+
+    // Create the directories as need be.
+    if (!is_dir($output)){
+      mkdir($output, 0755, true);
+    }
+
+    // Now create our file.
+    $this->filePath = $output . '/' . $file . '.md';
+    if (!file_exists($this->filePath)) {
+      $file = fopen($this->filePath, 'wb');
+      fwrite($file, '');
+      fclose($file);
+    }
+
+    // URL key is no longer needed.
+    unset($this->currentRow[$this->urlKey]);
+  }
+
+  /**
+   * Generate the front matter.
+   */
+  protected function generateFrontMatter() {
+    // Set our title then banish it.
+    $title = $this->currentRow[$this->titleKey];
+    unset($this->currentRow[$this->titleKey]);
+
+    $file = fopen($this->filePath, 'wb');
+    fwrite($file, "---\n");
+    fwrite($file, "title: " . $title . "\n");
+
+    // Write all other keys.
+    foreach ($this->currentRow as $key => $value) {
+      if ($key === $this->bodyKey) {
+        continue;
+      }
+
+      fwrite($file, $this->header[$key] . ": " . $value . "\n");
+      unset($this->currentRow[$key]);
+    }
+
+    fwrite($file, "---\n");
+    fclose($file);
+  }
+
+  /**
+   * Converts our html to markdown
+   */
+  protected function generateContent() {
+    $body = $this->currentRow[$this->bodyKey];
+
+    // If textile formatted.
+    if ($this->textileFormatted) {
+      $body = (new Parser())->parse($body);
+    }
+
+    // Now write the body content.
+    $file = fopen($this->filePath, 'ab');
+    fwrite($file, "\n");
+    fwrite($file, (new ConverterExtra)->parseString($body));
+    fclose($file);
+  }
+}
